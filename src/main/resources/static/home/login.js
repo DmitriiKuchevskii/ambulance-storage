2aac69f727873825cd1d5e05e4e6a209be520f88
Dmitrii Kuchevskii <dmitrii@kuchevskii.com>
2019-12-11 17:43:24 +0000

    New UI

--- src/main/resources/static/home/login.js
+++ src/main/resources/static/home/login.js
@@ -1,24 +1,83 @@
+document.getElementById("submitBtn").addEventListener("click", login);
 var xhttp = new XMLHttpRequest();
 function login(){
-    var logpass = {username:"", password:""};
+    let logpass = {username:"", password:""};
     logpass.username = document.querySelector("input[name='uname']").value;
     logpass.password = document.querySelector("input[name='psw']").value;
-    var url = "https://kuchevskii.com/api/Login";
+    let url = "https://kuchevskii.com/api/Login";
     xhttp.open("POST", url ,true);
     xhttp.setRequestHeader("content-type", "application/json");
     xhttp.onreadystatechange = respose;
-    var str = JSON.stringify(logpass);
+    let str = JSON.stringify(logpass);
     xhttp.send(str);
 }
 function respose(){
     if(xhttp.readyState == 4 && xhttp.status == 200){
         console.log(xhttp.responseText);
         createCookie();
+        window.location.replace("https://kuchevskii.com/home/authorised/cards.html")
     }
 }
 /*create cookie*/
 function createCookie(){
-    let name = "token";
+    let name = "JWT";
     let value = xhttp.responseText;
-    document.cookie = encodeURIComponent(name) + '=' + encodeURIComponent(value);
+    document.cookie = encodeURIComponent(name) + '=' + encodeURIComponent(value)
+       + "; path=/; domain=kuchevskii.com; secure";
 }
+/*
+var ebachObj = {   
+    "date":"1993-10-10",
+    "team":"team",
+    "code":"code",
+    "result":"result",
+    "sex":"MALE",
+    "age":25,
+    "fullName":"Full Name",
+    "address":"address",
+    "regularPatient": false,
+    "homeless": true,
+    "data":"data"
+};
+
+var ebachJSON = JSON.stringify(ebachObj);
+
+function getCookie(name) {
+    let matches = document.cookie.match(new RegExp(
+      "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+function uniResponse(){
+    if(xhttp.readyState == 4 && xhttp.status == 200){
+        console.log(xhttp.responseText);
+    }
+}
+function addEbach(){
+    let url = "https://kuchevskii.com:4782/api/Add";
+    let cookieToken = getCookie("JWT");
+    let token = JSON.parse(cookieToken).token;
+    xhttp.open("POST", url ,true);
+    xhttp.setRequestHeader("JWT", token);
+    xhttp.setRequestHeader("content-type", "application/json");
+    xhttp.onreadystatechange = uniResponse;
+    xhttp.send(ebachJSON);
+}
+function getAll(){
+    let url = "https://kuchevskii.com:4782/api/GetAll";
+    let cookieToken = getCookie("JWT");
+    let token = JSON.parse(cookieToken).token;
+    xhttp.open("GET", url ,true);
+    xhttp.setRequestHeader("JWT", token);
+    xhttp.setRequestHeader("content-type", "application/json");
+    xhttp.onreadystatechange = parseAll;
+    xhttp.send();
+}
+var patientArray;
+function parseAll(){
+    if(xhttp.readyState == 4 && xhttp.status == 200){
+        patientArray = JSON.parse(xhttp.responseText);
+        return patientArray;
+    }
+}
+*/

f400199d3983e9e3e8bd6268393a1498b0994398
Dmitrii Kuchevskii <dmitrii@kuchevskii.com>
2019-12-11 18:28:09 +0000

    Fixed token read from  cookies

--- src/main/java/com/ambulance/config/SecurityConfig.java
+++ src/main/java/com/ambulance/config/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -70,6 +71,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+            .ignoring()
+            .antMatchers(addAll(WEB_UI_PUBLICLY_AVAILABLE, API_PUBLICLY_AVAILABLE));
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
             .httpBasic().disable()
@@ -91,7 +99,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .and()
                 .authorizeRequests()
                 .expressionHandler(webExpressionHandler())
-                .antMatchers(addAll(WEB_UI_PUBLICLY_AVAILABLE, API_PUBLICLY_AVAILABLE)).permitAll()
                 .antMatchers(addAll(WEB_UI_ADMIN_ONLY_AVAILABLE, API_ADMIN_ONLY_AVAILABLE)).hasAuthority(ROLE_ADMIN)
                 .antMatchers(addAll(WEB_UI_USER_ONLY_AVAILABLE, API_USER_ONLY_AVAILABLE)).hasAuthority(ROLE_USER)
                 .anyRequest().denyAll()
--- src/main/java/com/ambulance/security/jwt/JwtTokenAuthenticationFilter.java
+++ src/main/java/com/ambulance/security/jwt/JwtTokenAuthenticationFilter.java
@@ -4,24 +4,30 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.WebUtils;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Optional;
 
 @AllArgsConstructor
 @Slf4j
 public class JwtTokenAuthenticationFilter extends OncePerRequestFilter {
 
-    private static final String JWT_TOKEN_HEADER = "JWT";
+    private static final String JWT_TOKEN_NAME = "JWT";
 
     private final JwtTokenProvider jwtTokenProvider;
 
     @Override
     protected void doFilterInternal(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws ServletException, IOException {
-        String token = req.getHeader(JWT_TOKEN_HEADER);
+        String token = Optional.ofNullable(WebUtils.getCookie(req, JWT_TOKEN_NAME))
+                .map(Cookie::getValue)
+                .orElse(req.getHeader(JWT_TOKEN_NAME));
+        log.info("token = {}", token);
         if (token != null  && jwtTokenProvider.validateToken(token)) {
             SecurityContextHolder.getContext().setAuthentication(jwtTokenProvider.getAuthentication(token));
         }
